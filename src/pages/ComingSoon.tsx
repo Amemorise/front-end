56a137488d9e6cba06599c07995c0f701b3433ed
Kofi Twum-Ampofo <pktwum96@gmail.com>
2022-07-08 18:09:17 +0100

    fix

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -5,10 +5,12 @@ import WebTitle from "../components/WebTitle";
 
 const ComingSoon = () => {
     return (
-        <div className="d-flex">
+        <div className="d-flex" style={{ height: "100vh", width: "100vw", background: "#1b3b6f", alignItems: "center", justifyContent: "center" }}>
             <div style={{ textAlign: "center" }}>
                 <WebTitle color={"white"} />
-                <Typography variant={"h6"}>COMING SOON</Typography>
+                <Typography variant={"h6"} color={"white"}>
+                    COMING SOON
+                </Typography>
             </div>
         </div>
     );
