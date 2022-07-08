332a36db2fabd9cb8d5e5238a840851fb9e6701d
pktwum96 <35983270+pktwum96@users.noreply.github.com>
2022-07-08 17:10:44 +0000

    Build: (56a1374) fix

--- src/pages/MyProfile.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import { setPageTitle } from "../helpers/helpers";
-
-const MyProfile = () => {
-    useEffect(() => setPageTitle("Profile"), []);
-    return <div>MyProfile</div>;
-};
-
-export default MyProfile;
--- src/pages/Settings.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import { setPageTitle } from "../helpers/helpers";
-
-const Settings = () => {
-    useEffect(() => setPageTitle("Settings"), []);
-    return <div>Settings</div>;
-};
-
-export default Settings;
