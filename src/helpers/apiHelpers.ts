b0d1a23c00648a660db53ee77d829d1762d7dedd
Kofi Twum-Ampofo <pktwum96@gmail.com>
2022-07-10 14:58:52 +0100

    fix base app

--- src/helpers/apiHelpers.ts
+++ src/helpers/apiHelpers.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:8080",
     withCredentials: true,
 });
 
